1835a7836560c1b899a1117dc288fea1ea9b7dff
Eric Schles <erschles@microsoft.com>
2018-07-04 19:47:35 -0400

    adding back train.csv.dvc

---
+++ train_model.py
@@ -0,0 +1,12 @@
+from sklearn import linear_model
+import pandas as pd
+import pickle
+
+train = pd.read_csv("data/train.csv")
+test = pd.read_csv("data/test.csv")
+
+lin_reg = linear_model.LinearRegression()
+lin_reg.fit(train[["A", "B"]], train["C"])
+
+with open("model.p", "wb") as output_file:
+    pickle.dump(lin_reg, output_file)
