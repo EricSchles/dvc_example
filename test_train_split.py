23b73a60b43b2d078ab07c13575c457348886cd0
Eric Schles <erschles@microsoft.com>
2018-07-04 20:47:19 -0400

    change test train split

--- test_train_split.py
+++ test_train_split.py
@@ -4,7 +4,7 @@ from sklearn.model_selection import train_test_split
 df = pd.read_csv("data/data.csv")
 X, y = df[["A","B"]], df["C"]
 X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.33, random_state=42)
+    X, y, test_size=0.45, random_state=42)
 
 train = pd.concat([X_train, y_train], axis=1)
 test = pd.concat([X_test, y_test], axis=1)
