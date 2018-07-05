5adf6eae077f8b22524066d89d46029b4e0f17d6
Eric Schles <erschles@microsoft.com>
2018-07-04 20:50:18 -0400

    got some better results

--- train_model.py
+++ train_model.py
@@ -5,7 +5,7 @@ import pickle
 train = pd.read_csv("data/train.csv")
 test = pd.read_csv("data/test.csv")
 
-lin_reg = linear_model.LinearRegression()
+lin_reg = linear_model.Lasso(alpha=0.1)
 lin_reg.fit(train[["A", "B"]], train["C"])
 
 with open("model.p", "wb") as output_file:
